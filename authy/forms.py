4fec41c2f3269784ad46fdb9f4fd968ac023586f
Sayat <azhen03@mail.ru>
2023-05-14 15:52:15 +0600

    update sfksdvkas

--- authy/forms.py
+++ authy/forms.py
@@ -23,7 +23,7 @@ class UserRegisterForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'prompt srch_explore'}), max_length=50, required=True)
     # username = forms.EmailInput(widget=forms.TextInput(attrs={'placeholder': 'Username'}), max_length=50, required=True)
 
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'pattern':'.+@stu\.sdu\.edu\.kz', 'class': 'prompt srch_explore', 'title':'Write your SDU email address'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'prompt srch_explore'}))
     # email = forms.EmailField()
